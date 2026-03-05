680348a80f84f9994e33c2f56e27d6a991f8bbeb
Dimitar Radenkov <dimitar.radenkov1@gmail.com>
2026-03-05 13:53:57 +0200

    Refactor AppVersionService to encapsulate version retrieval logic

--- SnippingTool/Services/AppVersionService.cs
+++ SnippingTool/Services/AppVersionService.cs
@@ -1,9 +1,24 @@
+using System.Diagnostics;
 using System.Reflection;
 
 namespace SnippingTool.Services;
 
 public sealed class AppVersionService : IAppVersionService
 {
-    public Version Current { get; } =
-        Assembly.GetEntryAssembly()?.GetName().Version ?? new Version(0, 0, 0);
+    public Version Current { get; } = GetCurrentVersion();
+
+    private static Version GetCurrentVersion()
+    {
+        var location = Assembly.GetEntryAssembly()?.Location;
+        if (!string.IsNullOrEmpty(location))
+        {
+            var fvi = FileVersionInfo.GetVersionInfo(location);
+            if (Version.TryParse(fvi.FileVersion, out var fileVersion))
+            {
+                return fileVersion;
+            }
+        }
+
+        return Assembly.GetEntryAssembly()?.GetName().Version ?? new Version(0, 0, 0);
+    }
 }
