47bb1c1d69f9654ac1fce526445d2262fa7141be
Dimitar Radenkov <dimitar.radenkov1@gmail.com>
2026-04-04 16:47:25 +0300

    Add using for Media.Imaging in RecordingAnnotation
    Added System.Windows.Media.Imaging using directive to OverlayWindow.RecordingAnnotation.cs to enable bitmap image handling within recording annotation logic.

--- SnippingTool/OverlayWindow.RecordingAnnotation.cs
+++ SnippingTool/OverlayWindow.RecordingAnnotation.cs
@@ -1,6 +1,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 using Microsoft.Extensions.Logging;
